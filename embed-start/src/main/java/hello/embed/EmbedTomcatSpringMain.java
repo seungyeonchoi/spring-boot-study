43a1cf8778205242ea5ee553172f763363ec9111
seungyeonchoi <seungyeonchoi@worksmobile.com>
2023-04-16 12:58:43 +0900

    내장 톰캣3 - 스프링
    - 내장 톰캣 라이브러리를 이용해, 자바코드로 스프링 컨테이너를 디스패쳐 서블릿으로 연결하는 예제

---
+++ embed-start/src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -0,0 +1,39 @@
+package hello.embed;
+
+import hello.servlet.HelloServlet;
+import hello.spring.HelloConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class EmbedTomcatSpringMain {
+
+    public static void main(String[] args) throws LifecycleException {
+        System.out.println("EmbedTomcatSpringMain.main");
+
+        // 톰캣 설정
+        Tomcat tomcat = new Tomcat();
+        Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        // 스프링 컨테이너 생성
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(HelloConfig.class); // 필요한 빈 등록
+
+        // 스프링 MVC 디스패처 서블리 생성, 스프링 컨테이너 연결
+        DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+
+        // 디스패처 서블릿 등록
+        Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "dispatcher", dispatcher);
+        context.addServletMappingDecoded("/", "dispatcher");
+
+        // 톰캣 실행
+        tomcat.start();
+        tomcat.getServer().await(); // 이 코드가 없으면, 실행된 tomcat 서버가 바로 종료되므로 수정
+    }
+}
